f912e9b789314d8c807ee6d3ca3e8a20df92e120
Saksham Dhuria <107601836+sakshamdhuria@users.noreply.github.com>
2024-06-10 08:10:14 -0700

    Update omp.cpp

--- benchmarks/cholesky/omp.cpp
+++ benchmarks/cholesky/omp.cpp
@@ -1,58 +1,63 @@
 #include "cholesky.hpp"
 #include <omp.h>
 
+// Function for Cholesky decomposition using OpenMP
+void choleskyOMP(std::vector<std::vector<double>>& matrix, 
+                 unsigned num_threads) {
+  int n = matrix.size();
 
-// TODO: indent 2 space 
-//       use up to 80 characteris per line
-// you can take a look at Google C++ code style guideline: https://google.github.io/styleguide/cppguide.html
-
-void choleskyOMP(std::vector<std::vector<double>>& matrix, unsigned num_threads) {
-    int n = matrix.size();
-
-    #pragma omp parallel num_threads(num_threads)
+  #pragma omp parallel num_threads(num_threads)
+  {
+    #pragma omp single
     {
-        #pragma omp single
+      for (int j = 0; j < n; j++) {
+        // Diagonal element
+        #pragma omp task depend(inout: matrix[j][j])
         {
-            for (int j = 0; j < n; j++) {
-                // Diagonal element
-                #pragma omp task depend(inout: matrix[j][j])
-                {
-                    matrix[j][j] = sqrt(matrix[j][j]);
-                }
+          matrix[j][j] = sqrt(matrix[j][j]);
+        }
 
-                // non-diag elements
-                for (int i = j + 1; i < n; i++) {
-                    #pragma omp task depend(in: matrix[j][j]) depend(inout: matrix[i][j])
-                    {
-                        matrix[i][j] /= matrix[j][j];
-                    }
-                }
+        // Non-diagonal elements
+        for (int i = j + 1; i < n; i++) {
+          #pragma omp task depend(in: matrix[j][j]) \
+                           depend(inout: matrix[i][j])
+          {
+            matrix[i][j] /= matrix[j][j];
+          }
+        }
 
-                for (int i = j + 1; i < n; i++) {
-                    for (int k = j + 1; k <= i; k++) {
-                        #pragma omp task depend(in: matrix[i][j], matrix[k][j]) depend(inout: matrix[i][k])
-                        {
-                            matrix[i][k] -= matrix[i][j] * matrix[k][j];
-                        }
-                    }
-                }
+        for (int i = j + 1; i < n; i++) {
+          for (int k = j + 1; k <= i; k++) {
+            #pragma omp task depend(in: matrix[i][j], matrix[k][j]) \
+                             depend(inout: matrix[i][k])
+            {
+              matrix[i][k] -= matrix[i][j] * matrix[k][j];
             }
+          }
         }
+      }
     }
+  }
 
-    // make it L.T
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            matrix[i][j] = 0.0;
-        }
+  // Make it lower triangular
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      matrix[i][j] = 0.0;
     }
+  }
 }
 
-std::chrono::microseconds measure_time_OMP(
-  const std::vector<std::vector<int>>& matrix, unsigned num_threads
-){
-    auto beg = std::chrono::high_resolution_clock::now();
-    choleskyOMP(matrix, num_threads);
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
+// Measure time for OpenMP Cholesky decomposition
+std::chrono::microseconds measure_time_OMP
+    (const std::vector<std::vector<double>>& matrix, 
+    unsigned num_threads) {
+
+  auto beg = std::chrono::high_resolution_clock::now();
+
+  std::vector<std::vector<double>> matrix_copy = matrix;  // Make a copy of the matrix
+  choleskyOMP(matrix_copy, num_threads);
+
+  auto end = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
+
 }
